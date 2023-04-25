be6224ada1810dfcb4e13bfe9a16f62418de9495
calumLaw <119591322+calumLaw@users.noreply.github.com>
2023-04-25 11:36:23 +0100

    Initial commit of the GPT3.5 CLI
    replace #API KEY HERE with your API key from OpenAI.
    
    The chat history keeps one full instance of your chat even when you close the CLI. This has not been adapted to manage multiple chat instances.

---
+++ chatgpt.py
@@ -0,0 +1,67 @@
+import os
+import openai
+import json
+from dotenv import load_dotenv
+
+# Load the API key from the .env file
+load_dotenv()
+api_key = #API_KEY_HERE
+
+# Initialize the OpenAI API client
+openai.api_key = api_key
+
+# Function to load conversation history from a JSON file
+def load_conversation_history(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, "r") as f:
+            return json.load(f)
+    else:
+        return []
+
+# Function to save conversation history to a JSON file
+def save_conversation_history(file_path, conversation_history):
+    with open(file_path, "w") as f:
+        json.dump(conversation_history, f, indent=2)
+
+# Function to send a message to GPT-3.5 Turbo and receive a response
+def chat_gpt(message, conversation_history=None):
+    model_engine = "gpt-3.5-turbo"
+
+    if conversation_history is None:
+        conversation_history = []
+
+    conversation_history.append({"role": "system", "content": "You are a helpful assistant."})
+    conversation_history.append({"role": "user", "content": message})
+
+    response = openai.ChatCompletion.create(
+        model=model_engine,
+        messages=conversation_history,
+        temperature=0.5,
+        max_tokens=100
+    )
+
+    chat_response = response.choices[0].message['content'].strip()
+    return chat_response
+
+# Main loop for cmd line interaction
+def main():
+    print("Welcome to ChatGPT CLI: Type 'exit' to quit")
+    conversation_history_file = "conversation_history.json"
+    conversation_history = load_conversation_history(conversation_history_file)
+
+    while True:
+        # Get user input
+        user_message = input("You: ")
+
+        # Check if the user wants to quit
+        if user_message.lower() == 'exit':
+            break
+
+        # Call the chat_gpt function and print the response
+        response = chat_gpt(user_message, conversation_history)
+        conversation_history.append({"role": "assistant", "content": response})
+        save_conversation_history(conversation_history_file, conversation_history)
+        print(f"ChatGPT: {response}")
+
+if __name__ == '__main__':
+    main()
